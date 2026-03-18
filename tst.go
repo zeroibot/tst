cc78943f87d3bdb79247cdc10a8868c556b92092
roidaradal <roidaradal@gmail.com>
2026-03-18 11:06:28 +0800

    Add P2W2

--- tst.go
+++ tst.go
@@ -14,6 +14,13 @@ type P2W1[I1, I2, R any] struct {
 	W1 R
 }
 
+type P2W2[I1, I2, R1, R2 any] struct {
+	P1 I1
+	P2 I2
+	W1 R1
+	W2 R2
+}
+
 type P3W1[I1, I2, I3, R any] struct {
 	P1 I1
 	P2 I2
@@ -42,6 +49,15 @@ func AllP2W1[I1, I2, R any](t *testing.T, testCases []P2W1[I1, I2, R], name stri
 	for _, x := range testCases {
 		actual := testFn(x.P1, x.P2)
 		assert(t, name, actual, x.W1)
+	}
+}
+
+// AllP2W2 tests all P2W2 test cases
+func AllP2W2[I1, I2, R1, R2 any](t *testing.T, testCases []P2W2[I1, I2, R1, R2], name string, testFn func(I1, I2) (R1, R2), assert1 assertFn[R1], assert2 assertFn[R2]) {
+	for _, x := range testCases {
+		actual1, actual2 := testFn(x.P1, x.P2)
+		assert1(t, name, actual1, x.W1)
+		assert2(t, name, actual2, x.W2)
 	}
 }
 
