2f7024cf860c33b5036f81050e2106c0ff003bb0
roidaradal <roidaradal@gmail.com>
2026-03-18 09:58:48 +0800

    Add P3W1

--- tst.go
+++ tst.go
@@ -14,6 +14,13 @@ type P2W1[I1, I2, R any] struct {
 	W1 R
 }
 
+type P3W1[I1, I2, I3, R any] struct {
+	P1 I1
+	P2 I2
+	P3 I3
+	W1 R
+}
+
 // AllCompare1 tests 1 pair of [actual, want] from the given generic test cases
 func AllCompare1[T, R any](t *testing.T, testCases []T, name string, testFn func(T) (R, R), assert assertFn[R]) {
 	for _, x := range testCases {
@@ -34,6 +41,14 @@ func AllP1W1[I, R any](t *testing.T, testCases []P1W1[I, R], name string, testFn
 func AllP2W1[I1, I2, R any](t *testing.T, testCases []P2W1[I1, I2, R], name string, testFn func(I1, I2) R, assert assertFn[R]) {
 	for _, x := range testCases {
 		actual := testFn(x.P1, x.P2)
+		assert(t, name, actual, x.W1)
+	}
+}
+
+// AllP3W1 tests all P3W1 test cases
+func AllP3W1[I1, I2, I3, R any](t *testing.T, testCases []P3W1[I1, I2, I3, R], name string, testFn func(I1, I2, I3) R, assert assertFn[R]) {
+	for _, x := range testCases {
+		actual := testFn(x.P1, x.P2, x.P3)
 		assert(t, name, actual, x.W1)
 	}
 }
